62270ca99edfab516e50ba7c8c761d33dbf31a78
Pablo Garrido <pablogs9@gmail.com>
2022-03-04 09:18:43 +0100

    Add -Werror (#28)
    * Add Werror only when building tests
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    
    * Fix warning
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>

--- src/type_utilities.c
+++ src/type_utilities.c
@@ -93,7 +93,7 @@ void print_type_info(
 
   if (level == 0) {
     snprintf(
-      buffer, sizeof(buffer), "%sIntrospection for %s/%s - %d members, %zd B\n",
+      buffer, sizeof(buffer), "%sIntrospection for %s/%s - %zu members, %zu B\n",
       indent_buffer.data,
       members->message_namespace_,
       members->message_name_,
